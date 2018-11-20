9582fcf4e64af126037faea4ece94986b8051f5e
leanfj <leanfj@hotmail.com>
2018-11-20 09:35:16 -0200

    modificação de default value para song count

--- db/migrate/20181120113156_create_songs.rb
+++ db/migrate/20181120113156_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.string :title
-      t.integer :played_count
+      t.integer :played_count, defualt: 0
       t.references :album, foreign_key: true
 
       t.timestamps
